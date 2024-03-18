dbf035c1a9c2e8ce4ff3f34876f7a3ab2fd4b1ec
puntonemo <code@puntonemo.com>
2024-03-18 07:11:21 +0100

    feat: refactor

--- src/modules/code-generator/code-generator.controller.ts
+++ src/modules/code-generator/code-generator.controller.ts
@@ -6,37 +6,27 @@ import * as path from 'path';
 export class CodeGeneratorController {
     @Get()
     @Render('code-generator/index')
-    default(): object {
-        return { title: 'Title', subtitle: 'Subtitle' };
-    }
-    @Get('model')
-    @Render('code-generator/model')
     model(): object {
         return;
     }
-    @Post('model')
-    @Render('code-generator/model')
+    @Post()
+    @Render('code-generator/index')
     modelGenerate(@Body() form): object {
         const config = JSON.parse(form.config);
 
         if (form.files) {
-            codegen(form.files, config);
+            codegen(form.files);
         }
         return { config };
     }
 }
 
-const codegen = (fullcode: string, config: any) => {
-    const modelName = config.model_name;
-    console.log(modelName);
-
-    /* \/\*\*\*\sbegin of\s(.[^\*]*) */
-
+const codegen = (fullcode: string) => {
     const reModelPath = /\/\/\s*model_path\s*=*(.[^\r\n]*)\r*\n*/g;
     const rerModelPath = [...fullcode.matchAll(reModelPath)];
 
-    console.log(rerModelPath);
     if (rerModelPath.length != 1) return;
+
     const fragmentPath = rerModelPath[0][1].trim();
     const regexp = /\/\*\*\*\sbegin of\s(.[^\*]*)/g;
     const array = [...fullcode.matchAll(regexp)];
@@ -55,17 +45,16 @@ const extractFragment = (
     const endOfBlockText = `end of ${fragmentName}`;
     const endOfCommentText = '***/';
     const endOfBlock = fullcode.indexOf(endOfBlockText, index);
-    //const fragmentPath = '/src/model';
     const endOfComment = fullcode.indexOf(endOfCommentText, index);
     if (endOfBlock > 0 && endOfComment > 0) {
         const codeBlock = fullcode.substring(endOfComment + 6, endOfBlock - 4);
-        //console.log(codeBlock);
+
         const filePath = path.join(
             process.env.PWD || process.env.INIT_CWD,
             fragmentPath
         );
         const fileName = path.join(filePath, `/${fragmentName}`);
-        console.log(fileName);
+
         fs.mkdirSync(filePath, { recursive: true } as any);
         fs.writeFileSync(fileName, codeBlock);
     }
