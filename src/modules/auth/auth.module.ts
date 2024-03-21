5ffcfe669d95d6171ca73b9b40f2b94487b50c6f
puntonemo <code@puntonemo.com>
2024-03-21 17:10:07 +0100

    auth module refactor

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from '@lib/auth/strategy/jws.strategy';
+import { JwtStrategy } from 'src/modules/auth/strategies/jws.strategy';
 import { UsersRepository } from '@model/users';
 import { UserRolesRepository } from '@model/user-roles';
 import { DatabaseModule } from '@lib/database';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GoogleController } from './google/google.controller';
 import { GoogleService } from './google/google.service';
-import { GoogleStrategy } from '@lib/auth/strategy/google.strategy';
+import { GoogleStrategy } from 'src/modules/auth/strategies/google.strategy';
 import { MicrosoftController } from './microsoft/microsoft.controller';
 import { MicrosoftService } from './microsoft/microsoft.service';
-import { MicrosoftStrategy } from '@lib/auth/strategy/microsoft.strategy';
+import { MicrosoftStrategy } from 'src/modules/auth/strategies/microsoft.strategy';
 
 @Module({
     imports: [
