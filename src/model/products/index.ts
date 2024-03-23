257da7fad7b8522a789e7d98b486d8a9484c9cdb
puntonemo <code@puntonemo.com>
2024-03-23 08:18:57 +0100

    fix: removed Products sample model

--- src/model/products/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './products.entity';
-export * from './products.repository.mongodb';
-export * from './products.dto.create';
-export * from './products.dto.find';
-export * from './products.dto.update';
--- src/model/products/products.dto.update.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductDto } from '.';
-
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
